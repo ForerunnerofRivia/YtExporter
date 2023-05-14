1a01ce70e7994c702eca134e6c8616254d392544
BastienSecdevops <120526012+BastienSecdevops@users.noreply.github.com>
2023-05-14 22:18:48 +0200

    rg

--- YtExporter.py
+++ YtExporter.py
@@ -7,11 +7,21 @@ parser = SafeConfigParser()
 parser.read('./auth.cfg')
 
 driverPath = parser.get('DRIVERPATH','driverPath')
+login = parser.get('CREDENTIAL','login')
+password = parser.get('CREDENTIAL','password')
+
 
 playlistToExport = "https://www.youtube.com/playlist?list=LL"
 
+
 driver = webdriver.Chrome()
 
+
+driver.get("https://www.youtube.com/")
+time.sleep(2000)
+
+login_button = driver.find_element_by_xpath('//*[@id="buttons"]/ytd-button-renderer/a')
+login_button.click()
 
 
 time.sleep(20)
